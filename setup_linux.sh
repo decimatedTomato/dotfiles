4581b8df15b86307acfb9186d5c90c4d5f42a086
decimatedTomato <87151993+decimatedTomato@users.noreply.github.com>
2025-11-16 16:39:50 +0100

    Changed package_install into a function rather than a variable containing a command
    Fixed cmake_grep alias

--- setup_linux.sh
+++ setup_linux.sh
@@ -12,9 +12,9 @@ install_fzf() {
 }
 
 install_c() {
-	$package_install gcc clang make cmake
-	$package_install g++ \
-		|| $package_install gcc-c++
+	package_install gcc clang make cmake
+	package_install g++ \
+		|| package_install gcc-c++
 }
 
 install_rust() {
@@ -35,21 +35,33 @@ install_vim_plug() {
 }
 
 install_tldr() {
-	$package_install tldr \
-		|| $package_install tlrc \
+	package_install tldr \
+		|| package_install tlrc \
 		|| sudo cargo install tldrx && echo 'alias tldr=tldrx' >> ~/.bashrc_local
 }
 
-if [ -x "$(command -v apt-get)" ]; then
-	package_install='sudo apt-get -y install'
-elif [ -x "$(command -v zypper)" ]; then
-	package_install='sudo zypper -y install'
-elif [ -x "$(command -v pacman)" ]; then
-	package_install='sudo pacman --noconfirm -S'
-else
-	echo 'Package manager not found.'
-	exit 1
-fi
+install_netcat() {
+	package_install netcat \
+		|| package_install netcat-openbsd
+}
+
+get_package_install_command() {
+	if [ -x "$(command -v apt-get)" ]; then
+		echo 'sudo apt-get -y install'
+	elif [ -x "$(command -v zypper)" ]; then
+		echo 'sudo zypper -y install'
+	elif [ -x "$(command -v pacman)" ]; then
+		echo 'sudo pacman --noconfirm -S'
+	else
+		echo 'Package manager not found.' >&2
+		exit 1
+	fi
+}
+
+install_command=$(get_package_install_command)
+package_install() {
+	$install_command "$@"
+}
 
 insert_dotfile .gitconfig
 insert_dotfile .bashrc
@@ -57,17 +69,17 @@ insert_dotfile .tmux.conf
 insert_dotfile .vimrc
 ln -sf $script_dir/.alias ~/.alias
 
-ask "Install *curl* to make network requestss?" && $package_install curl
+ask "Install *curl* to make network requests?" && package_install curl
 ask "Install some *c* toolchain stuff?" && install_c
 ask "Install the *rust* toolchain?" && install_rust
 ask "Install the *javascript* toolchain?" && install_javascript
-ask "Install the *ocaml* toolchain?" && $package_install ocaml utop
-ask "Install *tmux* the terminal multiplexer?" && $package_install tmux
+ask "Install the *ocaml* toolchain?" && package_install ocaml utop
+ask "Install *tmux* the terminal multiplexer?" && package_install tmux
 ask "Install *fzf* for fuzzy finding and shell completion?" && install_fzf
 ask "Install *vim-plug*, a simple vim plugin manager?" && install_vim_plug
 ask "Install *tldr* to explain common usages of commands?" && install_tldr
-ask "Install *jq* to edit json?" && $package_install jq
-ask "Install *tree* to display a filetree?" && $package_install tree
-ask "Install *tig* to interactively view the git history?" && $package_install tig
-ask "Install *htop* to interatively view processes?" && $package_install htop
-ask "Install *netcat* to interact with tcp/udp sockets?" && $package_install netcat
+ask "Install *jq* to edit json?" && package_install jq
+ask "Install *tree* to display a filetree?" && package_install tree
+ask "Install *tig* to interactively view the git history?" && package_install tig
+ask "Install *htop* to interatively view processes?" && package_install htop
+ask "Install *netcat* to interact with tcp/udp sockets?" && install_netcat
