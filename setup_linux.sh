4daaad971c12bb33f102e6055bab28a82d84663e
decimatedTomato <87151993+decimatedTomato@users.noreply.github.com>
2025-11-16 16:20:16 +0100

    Told rustup not to edit .profile and .bashrc since I already set those just in case

--- setup_linux.sh
+++ setup_linux.sh
@@ -18,7 +18,7 @@ install_c() {
 }
 
 install_rust() {
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 }
 
 install_javascript() {
